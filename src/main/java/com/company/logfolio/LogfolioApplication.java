4235339a5578ede880fb8e5233ee63e015f07f8f
bartoszkruba <bartosz.kruba@gmail.com>
2019-04-06 18:00:07 +0200

    Create Spring project for backend. Closes #1

---
+++ src/main/java/com/company/logfolio/LogfolioApplication.java
@@ -0,0 +1,13 @@
+package com.company.logfolio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogfolioApplication {
+
+   public static void main(String[] args) {
+      SpringApplication.run(LogfolioApplication.class, args);
+   }
+
+}
